7751dc2da9ed1432e4e793dcea6294e3dc80049f
Mike Damiano <100625630+mikedami@users.noreply.github.com>
2023-09-07 14:58:15 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,37 +1,31 @@
 var http = require('http'), 
     fs = require('fs'), 
-    port = 8080;
+    port = 8080,
+    url = require('url');
 
-/* Global variables */
 var listingData, server;
 
 var requestHandler = function(request, response) {
-  /*Investigate the request object. 
-    You will need to use several of its properties: url and method
-  */
-  //console.log(request);
 
-  /*
-    Your request handler should send listingData in the JSON format as a response if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
-
-    HINT: Explore the request object and its properties 
-    HINT: Explore the response object and its properties
-    https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
-    
-    HINT: Explore how callback's work 
-    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
-    
-    HINT: Explore the list of MIME Types
-    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-   
-    HINT: Explore mdn web docs for resources on how to use javascript.
-    Helpful example: if-else structure- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
-
-    */
+  if(request.method == 'GET' && request.url == '/listings'){
+    response.end(JSON.stringify(listingData));
+  } else{
+    response.end('Bad Gateway Error.')
+    response.statusCode = 404;
+  }
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
+
+  if(err){
+    throw err;
+  }
+  listingData = JSON.parse(data);
+  server = http.createServer(requestHandler);
+  server.listen(port, function(){
+    console.log('Server is listening on: http://localhost:'+port);
+  });
+});
   /*
     This callback function should save the data in the listingData variable, 
     then start the server. 
@@ -47,7 +41,6 @@ fs.readFile('listings.json', 'utf8', function(err, data) {
     /*this resource gives you an idea of the general format err objects and Throwing an existing object.
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw#throwing_an_existing_object
    */
-  
 
    //Save the data in the listingData variable already defined
   
@@ -55,6 +48,3 @@ fs.readFile('listings.json', 'utf8', function(err, data) {
   //Creates the server
   
   //Start the server
-
-
-});
